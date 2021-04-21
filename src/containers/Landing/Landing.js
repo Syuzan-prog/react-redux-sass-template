93a6167d063507ae157bf55a76350a940575370f
syuzan-prog <syuzanna.ant@gmail.com>
2021-04-21 15:34:16 +0400

    Remove navigation

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -7,7 +7,7 @@ import './Landing.scss';
 const Landing = () => {
     return (
         <div className="app-landing">
-            <Navigation/>
+            {/* <Navigation/> */}
             <h1 className="app-landing__title">React-Redux-Sass Boilerplate Project</h1>
             <Link className="app-landing__link" to="/redux-test">Test Redux {'>'}</Link>
             <span className="app-landing__copyright">&#169; Sokrat Poghosyan, {new Date().getFullYear()}</span>
